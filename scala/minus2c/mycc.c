283479563f3d607e49bcfd603b4c28ae89911776
James Thompson <james.thompson@essensys.tech>
2018-10-09 04:04:54 +0100

    specialise If Else

--- scala/minus2c/mycc.c
+++ scala/minus2c/mycc.c
@@ -134,9 +134,7 @@ void print_string_constant(Token *token) {
   printf("\"%s\"\n", token->data.lexeme);
 }
 
-void print_token_default(Token *token) {
-  printf("%s\n", token->data.lexeme);
-}
+void print_token_default(Token *token) { printf("%s\n", token->data.lexeme); }
 
 extern int yydebug;
 extern Ast *yyparse(void);
@@ -147,52 +145,38 @@ void set_debug(bool debug) { yydebug = debug ? 1 : 0; }
 
 Ast *get_ans() { return polyglot_from_ast(ans); }
 
-static void *java_util_ArrayDeque;
-static void *scala_util_Left_String;
-static void *scala_util_Right_Int;
-
 static void *mycc_CAst;
-static void *mycc_CAst$Token;
-static void *mycc_CAst$Node;
-static void *mycc_CAst$BinaryNode;
-
 static void *(*mycc_CAst$Token_new)(void *, void *);
 static void *(*mycc_CAst$Node_new)(void *, void *);
 static void *(*mycc_CAst$BinaryNode_new)(void *, void *, void *);
 
-static void *(*java_util_ArrayDeque_push)(void *);
-static void *(*java_util_ArrayDeque_pop)();
+static void *(*ArrayDeque_push)(void *);
+static void *(*ArrayDeque_pop)();
 
 static void *(*scala_util_Left_String_new)(void *);
 static void *(*scala_util_Right_Int_new)(int);
 
-static void *java_util_ArrayDeque_inst;
+static void *ArrayDeque_inst;
 
 void init_mycc_CAst() {
-  java_util_ArrayDeque = polyglot_java_type("java.util.ArrayDeque");
-
-  scala_util_Left_String = polyglot_java_type("scala.util.Left");
-  scala_util_Right_Int = polyglot_java_type("scala.util.Right");
 
   mycc_CAst = polyglot_java_type("mycc.CAst");
-  mycc_CAst$Token = polyglot_java_type("mycc.CAst$Token");
-  mycc_CAst$BinaryNode = polyglot_java_type("mycc.CAst$BinaryNode");
-  mycc_CAst$Node = polyglot_java_type("mycc.CAst$Node");
+  mycc_CAst$Token_new =
+      polyglot_get_member(polyglot_java_type("mycc.CAst$Token"), "apply");
+  mycc_CAst$Node_new =
+      polyglot_get_member(polyglot_java_type("mycc.CAst$Node"), "apply");
+  mycc_CAst$BinaryNode_new =
+      polyglot_get_member(polyglot_java_type("mycc.CAst$BinaryNode"), "apply");
 
-  mycc_CAst$Token_new = polyglot_get_member(mycc_CAst$Token, "apply");
-  mycc_CAst$Node_new = polyglot_get_member(mycc_CAst$Node, "apply");
-  mycc_CAst$BinaryNode_new = polyglot_get_member(mycc_CAst$BinaryNode, "apply");
-
-  java_util_ArrayDeque_inst = polyglot_new_instance(java_util_ArrayDeque);
-  java_util_ArrayDeque_pop =
-      polyglot_get_member(java_util_ArrayDeque_inst, "pop");
-  java_util_ArrayDeque_push =
-      polyglot_get_member(java_util_ArrayDeque_inst, "push");
+  ArrayDeque_inst =
+      polyglot_new_instance(polyglot_java_type("java.util.ArrayDeque"));
+  ArrayDeque_pop = polyglot_get_member(ArrayDeque_inst, "pop");
+  ArrayDeque_push = polyglot_get_member(ArrayDeque_inst, "push");
 
   scala_util_Left_String_new =
-      polyglot_get_member(scala_util_Left_String, "apply");
+      polyglot_get_member(polyglot_java_type("scala.util.Left"), "apply");
   scala_util_Right_Int_new =
-      polyglot_get_member(scala_util_Right_Int, "apply");
+      polyglot_get_member(polyglot_java_type("scala.util.Right"), "apply");
 }
 
 void Ast_to_Scala(Ast *);
@@ -200,7 +184,7 @@ void Node_to_Scala(Node *);
 void BinaryNode_to_Scala(BinaryNode *);
 void Token_to_Scala(Token *);
 
-void *get_deque() { return java_util_ArrayDeque_inst; }
+void *get_deque() { return ArrayDeque_inst; }
 
 void Ast_to_Scala(Ast *ast) {
   if (NULL == ast || polyglot_is_null(ast)) {
@@ -222,40 +206,34 @@ void Ast_to_Scala(Ast *ast) {
   }
 }
 
+#define JAVA_STRING(expr) polyglot_from_string(expr, "UTF-8")
+
 void BinaryNode_to_Scala(BinaryNode *node) {
   Ast_to_Scala(node->right);
   Ast_to_Scala(node->left);
-  java_util_ArrayDeque_push(polyglot_from_string(named(&node->ast), "UTF-8"));
-  java_util_ArrayDeque_push(mycc_CAst$BinaryNode_new(
-      java_util_ArrayDeque_pop(), java_util_ArrayDeque_pop(),
-      java_util_ArrayDeque_pop()));
+  ArrayDeque_push(JAVA_STRING(named(&node->ast)));
+  ArrayDeque_push(mycc_CAst$BinaryNode_new(ArrayDeque_pop(), ArrayDeque_pop(),
+                                           ArrayDeque_pop()));
 }
 
 void Node_to_Scala(Node *node) {
   Ast_to_Scala(node->left);
-  java_util_ArrayDeque_push(polyglot_from_string(named(&node->ast), "UTF-8"));
-  java_util_ArrayDeque_push(mycc_CAst$Node_new(java_util_ArrayDeque_pop(),
-                                               java_util_ArrayDeque_pop()));
+  ArrayDeque_push(JAVA_STRING(named(&node->ast)));
+  ArrayDeque_push(mycc_CAst$Node_new(ArrayDeque_pop(), ArrayDeque_pop()));
 }
 
 void Token_to_Scala(Token *token) {
   switch (token->ast.type) {
   case CONSTANT:
-    java_util_ArrayDeque_push(scala_util_Right_Int_new(token->data.value));
-    java_util_ArrayDeque_push(
-        polyglot_from_string(named(&token->ast), "UTF-8"));
-    java_util_ArrayDeque_push(mycc_CAst$Token_new(java_util_ArrayDeque_pop(),
-                                                  java_util_ArrayDeque_pop()));
+    ArrayDeque_push(scala_util_Right_Int_new(token->data.value));
+    ArrayDeque_push(JAVA_STRING(named(&token->ast)));
+    ArrayDeque_push(mycc_CAst$Token_new(ArrayDeque_pop(), ArrayDeque_pop()));
     return;
   default:
-    java_util_ArrayDeque_push(
-        polyglot_from_string(token->data.lexeme, "UTF-8"));
-    java_util_ArrayDeque_push(
-        scala_util_Left_String_new(java_util_ArrayDeque_pop()));
-    java_util_ArrayDeque_push(
-        polyglot_from_string(named(&token->ast), "UTF-8"));
-    java_util_ArrayDeque_push(mycc_CAst$Token_new(java_util_ArrayDeque_pop(),
-                                                  java_util_ArrayDeque_pop()));
+    ArrayDeque_push(JAVA_STRING(token->data.lexeme));
+    ArrayDeque_push(scala_util_Left_String_new(ArrayDeque_pop()));
+    ArrayDeque_push(JAVA_STRING(named(&token->ast)));
+    ArrayDeque_push(mycc_CAst$Token_new(ArrayDeque_pop(), ArrayDeque_pop()));
     return;
   }
 }
--- scala/minus2c/src/main/scala/mycc/Ast.scala
+++ scala/minus2c/src/main/scala/mycc/Ast.scala
@@ -5,10 +5,13 @@ enum Types {
 }
 
 enum Ast {
+  case ø
   case Identifier(id: String)
   case Constant(value: Int)
   case StringLiteral(value: String)
   case Type(id: Types)
+  case If(cond: Ast, ifTrue: Ast)
+  case IfElse(cond: Ast, ifTrue: Ast, orElse: Ast)
   case Symbolic(kind: String, data: Either[String, Int])
   case Node1(kind: String, left: Ast)
   case Node2(kind: String, left: Ast, right: Ast)
